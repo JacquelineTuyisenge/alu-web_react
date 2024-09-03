f53a01e2c66734e2d07ec1120975f213a81b5b22
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-09-03 23:45:03 +0200

    readonly in interfaces

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,9 +1,20 @@
-interface Teacher {
+export interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
     [key: string]: any;
-}
-export default Teacher;
+};
+
+export interface Directors extends Teacher {
+    numberOfReports: number;
+};
+
+export interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+};
+
+export const printTeacher: PrintTeacherFunction = (firstName, lastName)=> {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
