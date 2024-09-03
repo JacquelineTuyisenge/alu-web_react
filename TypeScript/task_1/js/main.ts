9c8e58188ef8e4a8e5327d8a9ac629110e0a5151
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-09-04 00:05:31 +0200

    readonly in interfaces

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -18,3 +18,30 @@ export interface PrintTeacherFunction {
 export const printTeacher: PrintTeacherFunction = (firstName, lastName)=> {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+
+export interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+};
+
+export interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface
+};
+
+export class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    };
+
+    displayName(): string {
+        return this.firstName;
+    };
+}
